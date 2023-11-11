f8490d5a11ddd24bc5630e15caffa5650df6ef59
JaeHunJang <wuddlaa@gmail.com>
2023-11-12 00:54:38 +0900

    refactor(OrderValidator): 메뉴 존재여부 함수 enum 사용하도록 변경

--- src/main/java/christmas/util/validator/OrderValidator.java
+++ src/main/java/christmas/util/validator/OrderValidator.java
@@ -1,31 +1,20 @@
 package christmas.util.validator;
 
 import christmas.config.Constant;
+import christmas.config.Menu;
 import christmas.config.Message;
 import christmas.util.Util;
 
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class OrderValidator {
     private final String order;
-    private final Set<String> menuAppetizer = Set.of("양송이수프", "타파스", "시저샐러드");
-    private final Set<String> menuMain = Set.of("티본스테이크", "바비큐립", "해산물파스타", "크리스마스파스타");
-    private final Set<String> menuDessert = Set.of("초코케이크", "아이스크림");
-    private final Set<String> menuDrink = Set.of("제로콜라", "레드와인", "샴페인");
-
-    private final Set<String> menuAll = new HashSet<>();
 
     public OrderValidator(String order) {
         this.order = order;
-        menuAll.addAll(menuAppetizer);
-        menuAll.addAll(menuMain);
-        menuAll.addAll(menuDessert);
-        menuAll.addAll(menuDrink);
 
         validatePattern();
-        validateContainMenu();
+        validateExistMenu();
         validateOrderQuantity();
         validateOrderDuplicate();
     }
@@ -36,8 +25,12 @@ public class OrderValidator {
         }
     }
 
-    private void validateContainMenu() {
-        if (!menuAll.containsAll(generateMenuName())) {
+    private void validateExistMenu() {
+        generateMenuName().forEach(this::isExistMenu);
+    }
+
+    private void isExistMenu(String name) {
+        if (Menu.getMenu(name) == null) {
             throw new IllegalArgumentException(Message.ERROR_INPUT_ORDER);
         }
     }
