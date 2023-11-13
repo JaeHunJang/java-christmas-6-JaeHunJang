3ec14ea0eded43c9d6dd5ae5069903ec16ffbc4c
JaeHunJang <wuddlaa@gmail.com>
2023-11-13 14:19:38 +0900

    feat: 특별할인 추가 및 name 값 추가

--- src/main/java/christmas/config/Event.java
+++ src/main/java/christmas/config/Event.java
@@ -1,8 +1,15 @@
 package christmas.config;
 
 public enum Event {
-    CHRISTMAS,
-    WEEKDAY,
-    WEEKEND,
-    GIFT
+    CHRISTMAS("크리스마스 디데이 할인"),
+    WEEKDAY("평일 할인"),
+    WEEKEND("주말 할인"),
+    SPECIAL("특별 할인"),
+    GIFT("증정 이벤트");
+
+    private final String name;
+
+    Event(String name) {
+        this.name = name;
+    }
 }
